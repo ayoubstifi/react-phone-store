a40683bc0abbd1e3913ad42e5421dd30bdd7a571
ayoubstifi <stifiayoub@gmail.com>
2020-07-10 18:05:38 +0100

    Update context.js

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { storeProducts, detailProduct } from "./data";
 const ProductContext = React.createContext();
 //provider
