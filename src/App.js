a367ebee3ec8daa9a10a6538076d1ea37a7d3819
ayoubstifi <stifiayoub@gmail.com>
2020-07-10 18:19:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar.js";
 import ProductList from "./components/ProductList.js";
-import Product from "./components/Product.js";
 import Details from "./components/Details.js";
 import Cart from "./components/Cart/Cart.js";
 import Default from "./components/Default";
